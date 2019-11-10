24b426d4e08c96697783c85a5299dac791721d81
Hunain Arbani <hunain.arbani@yahoo.com>
2019-11-10 15:21:28 +0500

    adding student table structure

---
+++ mysql_project/studenttable.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `student_master` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `student_name` varchar(50) NOT NULL,
+ `email` varchar(50) NOT NULL,
+ `age` int(11) NOT NULL,
+ `marital_status` varchar(20) NOT NULL,
+ PRIMARY KEY (`id`)
+)
