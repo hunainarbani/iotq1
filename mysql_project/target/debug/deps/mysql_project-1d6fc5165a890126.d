183e11003e099452cb727f83c56f154fbea96537
Hunain Arbani <hunain.arbani@yahoo.com>
2019-11-10 15:07:21 +0500

    Commit Initial Project

---
+++ mysql_project/target/debug/deps/mysql_project-1d6fc5165a890126.d
@@ -0,0 +1,5 @@
+/home/hunain/Desktop/IOTQ1/mysql_project/target/debug/deps/mysql_project-1d6fc5165a890126: src/main.rs
+
+/home/hunain/Desktop/IOTQ1/mysql_project/target/debug/deps/mysql_project-1d6fc5165a890126.d: src/main.rs
+
+src/main.rs:
---
+++ mysql_project/target/debug/mysql_project.d
@@ -0,0 +1,1 @@
+/home/hunain/Desktop/IOTQ1/mysql_project/target/debug/mysql_project: /home/hunain/Desktop/IOTQ1/mysql_project/src/main.rs
